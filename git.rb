694f81411df7ce023252222f9512be77ce157da8
Kelsie He <hekelsie@gmail.com>
2019-10-03 09:55:41 -0600

    How I am feeling

--- git.rb
+++ git.rb
@@ -1,1 +1,4 @@
 puts "Coding is fun!!!"
+puts "I am very tired"
+puts "I'm also quite stressed because I have an exam tomorrow and a lot of projects due today that I haven't finished"
+puts "I'm also fAmIShEd and I want some of those raspberry cookies :(((("
